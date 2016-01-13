ff75cb88d44de873347d22b53f3d84981cd09b54
EmiPatoR <hrustic.emir@gmail.com>
2016-01-13 22:20:34 +0100

    Config Symfony en environement de DEV

--- src/ESC/MainBundle/Controller/DefaultController.php
+++ src/ESC/MainBundle/Controller/DefaultController.php
@@ -3,11 +3,13 @@
 namespace ESC\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ESCMainBundle:Default:index.html.twig');
+        $content = $this->get("templating")->render("ESCMainBundle:Default:index.html.twig");
+        return new Response($content);
     }
 }
